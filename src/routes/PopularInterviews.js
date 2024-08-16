0cbd227be86450cb7bed894a4b39f12c8c0c7f65
Rahul <chrahulofficial@gmail.com>
2024-08-16 20:20:02 +0530

    done

---
+++ src/routes/PopularInterviews.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const popularInterviewsRoute = express.Router();
+const users = require("../models/User");
+
+popularInterviewsRoute.get("/interviews/popular", async (req, res) => {
+  try {
+    const popularInterviews = await users.aggregate([
+      { $unwind: "$interviews" },
+      { $sort: { "interviews.Likes": -1 } },
+      {
+        $project: {
+          _id: 0,
+          "interviews._id": 1,
+          "interviews.company": 1,
+          "interviews.position": 1,
+          "interviews.experience": 1,
+          "interviews.Likes": 1,
+        },
+      },
+    ]);
+
+    res.status(200).json(popularInterviews);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = popularInterviewsRoute;
